57c6b5d0e4349edb8b6b4e08e8f5ede8c2d05d52
Maxde1 <maxdeniss56@gmail.com>
2022-01-25 15:46:00 +0200

    Initial commit

---
+++ src/binary_search/BinarySearch.java
@@ -0,0 +1,27 @@
+package binary_search;
+
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class BinarySearch {
+    public static int find(int arr[], int searchElement)  {
+        int start = 0;
+        int end = arr.length-1;
+        int middle = 0;
+        while (start <= end){
+            middle = (start+end)/2;
+
+            if (arr[middle] == searchElement) return middle;
+            else if (arr[middle] > searchElement) end = middle-1;
+            else if (arr[middle] < searchElement) start = middle+1;
+        }
+        return -1;
+//        throw new Exception("Element not found");
+    }
+
+    public static void main(String[] args) {
+        int [] arr = new int[]{1, 2, 3, 4, 5};
+        System.out.println(find(arr, 6));
+    }
+
+}
